21e8545e7e84e0e600d5222cc077de5cbd9a72c9
farbod-behnam <farbod.behnam@gmail.com>
2023-03-05 15:31:31 +0330

    add password encryption to user's password

--- src/main/java/com/hoaxify/hoaxify/services/UserService.java
+++ src/main/java/com/hoaxify/hoaxify/services/UserService.java
@@ -2,18 +2,24 @@ package com.hoaxify.hoaxify.services;
 
 import com.hoaxify.hoaxify.entities.AppUser;
 import com.hoaxify.hoaxify.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+
     }
 
     public AppUser save(AppUser appUser) {
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
         return userRepository.save(appUser);
     }
 }
